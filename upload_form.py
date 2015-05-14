ab2b082fc3873f8f3eac7799df64d721c359bae9
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-05-14 11:31:49 +0200

    remove comment block

--- upload_form.py
+++ upload_form.py
@@ -394,62 +394,6 @@ def query(filename, fileformat=None):
                          VDispMin,
                          VDispMax, RadMin, RadMax,
                          interactive=False)
-    
-#    UseSurf = (SurfDens > SurfMin) & (SurfDens < SurfMax)
-#    UseVDisp = (VDisp > VDispMin) & (VDisp < VDispMax)
-#    UseRad = (Rad > RadMin) & (Rad < RadMax)
-#    Use = UseSurf & UseVDisp & UseRad
-#    Obs = (~IsSim) & Use
-#    Sim = IsSim & Use
-#    
-#    UniqueAuthor = set(Author[Use])
-#    NUniqueAuthor = len(UniqueAuthor)
-#    
-#    #colors = random.sample(matplotlib.colors.cnames, NUniqueAuthor)
-#    colors = list(plt.cm.jet(np.linspace(0,1,NUniqueAuthor)))
-#    random.shuffle(colors)
-#    
-#    plt.loglog()
-#    markers = ['o','s']
-#    for iAu,color in zip(UniqueAuthor,colors) :
-#        UsePlot = (Author == iAu) & Use
-#        ObsPlot = ((Author == iAu) & (~IsSim)) & Use 
-#        SimPlot = ((Author == iAu) & (IsSim)) & Use
-#        if any(ObsPlot):
-#            plt.scatter(SurfDens[ObsPlot], VDisp[ObsPlot], marker=markers[0],
-#                        s=(np.log(np.array(Rad[ObsPlot]))-np.log(np.array(RadMin))+0.5)**3.,
-#                        color=color, alpha=0.5)
-#        if any(SimPlot):
-#            plt.scatter(SurfDens[SimPlot], VDisp[SimPlot], marker=markers[1],
-#                        s=(np.log(np.array(Rad[SimPlot]))-np.log(np.array(RadMin))+0.5)**3.,
-#                        color=color, alpha=0.5)
-#    if any(Obs):
-#        plt.scatter(SurfDens[Obs], VDisp[Obs], marker=markers[0],
-#                    s=(np.log(np.array(Rad[Obs]))-np.log(np.array(RadMin))+0.5)**3.,
-#                    facecolors='none', edgecolors='black',
-#                    alpha=0.5)
-#    if any(Sim):
-#        plt.scatter(SurfDens[Sim], VDisp[Sim], marker=markers[1],
-#                    s=(np.log(np.array(Rad[Sim]))-np.log(np.array(RadMin))+0.5)**3.,
-#                    facecolors='none', edgecolors='black',
-#                    alpha=0.5)
-#    plt.xlabel('$\Sigma$ [M$_{\odot}$ pc$^{-2}$]', fontsize=16)
-#    plt.ylabel('$\sigma$ [km s$^{-1}$]', fontsize=16)
-#
-#    ax = plt.gca()
-#    box = ax.get_position()
-#    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-#
-#    # Put a legend to the right of the current axis
-#    ax.legend(UniqueAuthor, loc='center left', bbox_to_anchor=(1.0, 0.5), prop={'size':12}, markerscale = .7, scatterpoints = 1)
-#
-##    plt.xlim((SurfMin.to(u.M_sun/u.pc**2).value,SurfMax.to(u.M_sun/u.pc**2).value))
-##    plt.ylim((VDispMin.to(u.km/u.s).value,VDispMax.to(u.km/u.s).value))
-#    plt.show()
-#    plt.savefig(os.path.join(app.config['OUTPUT_FOLDER'], FigureStrBase+NQuery+'.png'),bbox_inches='tight',dpi=150)
-##    plt.savefig(os.path.join(app.config['OUTPUT_FOLDER'], FigureStrBase+NQuery+'.pdf'),bbox_inches='tight',dpi=150)
-#    
-#    return render_template('show_plot.html', imagename='/'+FigureStrBase+NQuery+'.png')
 
 
 class InvalidUsage(Exception):
