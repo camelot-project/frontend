235770f3377432ea56fff5520a4e7561b538439a
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-05-17 00:16:01 +0200

    host!

--- upload_form.py
+++ upload_form.py
@@ -712,4 +712,4 @@ def handle_invalid_usage(error):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.getenv('PORT')))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT')))
